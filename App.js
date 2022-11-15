35182133e51c50eb120e1e15a3bfb0877d98de8e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-15 12:30:26 -0300

    Inclusão de texto

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TouchableOpacity, 
+  TextInput 
+} from 'react-native'
+
+import { FontAwesome } from '@expo/vector-icons'
 
 export default function App(){
 
@@ -7,6 +15,17 @@ export default function App(){
   return(
     <View style={styles.container}>
        <Text style={styles.title}>Tarefas</Text>
+
+       <View style={styles.containerInput}>
+        <TextInput
+          placeholder="Digite sua tarefa..."
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.buttonAdd}>
+          <FontAwesome name="plus" size={20} color="#fff" />
+        </TouchableOpacity>
+       </View>
     </View>
   )
 }
@@ -25,5 +44,8 @@ const styles = StyleSheet.create({
     marginTop: '5%',
     paddingStart: '5%',
     marginBottom: 12,
+  },
+  containerInput:{
+    flexDirection: 'row'
   }
 })
