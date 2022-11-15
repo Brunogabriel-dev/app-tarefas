bb67a2312ff72c6206c28d481b4e66b18247510b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-15 11:14:45 -0300

    Inclusão de texto

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { View, Text} from 'react-native'
 
 export default function App(){
   return(
-    <View style={{ flex:1, backgroundColor: '#121212'}}>
-      <Text>Bruno</Text>
+    <View style={{ flex:1, backgroundColor: '#121212', paddingTop: 28}}>
+      <Text style={{ fontSize: 28}}>Bruno</Text>
       <Text>Sujeito programador</Text>
     </View>
   )
