dfc8c895a51f334d4f5efcbd48be5598e1a83608
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-14 22:30:24 -0300

    Inclusão de texto

--- App.js
+++ App.js
@@ -1,2 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { View, Text} from 'react-native'
+
+export default function App(){
+  return(
+    <View style={{ display: 'flex'}}>
+      <Text>Bruno</Text>
+    </View>
+  )
+}
